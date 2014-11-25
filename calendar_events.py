e395168e05ede12f32aeb90f03c875c559985993
benboomz <benboomz@gmail.com>
2014-11-24 22:40:01 -0500

    Fixed minimum time so that start time is the current time for retrieving events.

--- calendar_events.py
+++ calendar_events.py
@@ -1,8 +1,43 @@
 calendarId = 'teamdanier@gmail.com'
 
 import google_calendar
-from datetime import datetime 
+from datetime import tzinfo, timedelta, datetime
 import pprint
+import time as _time
+
+STDOFFSET = timedelta(seconds = -_time.timezone)
+if _time.daylight:
+    DSTOFFSET = timedelta(seconds = -_time.altzone)
+else:
+    DSTOFFSET = STDOFFSET
+
+ZERO = timedelta(0)
+DSTDIFF = DSTOFFSET - STDOFFSET
+
+class LocalTimezone(tzinfo):
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return DSTOFFSET
+        else:
+            return STDOFFSET
+
+    def dst(self, dt):
+        if self._isdst(dt):
+            return DSTDIFF
+        else:
+            return ZERO
+
+    def tzname(self, dt):
+        return _time.tzname[self._isdst(dt)]
+
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, 0)
+        stamp = _time.mktime(tt)
+        tt = _time.localtime(stamp)
+        return tt.tm_isdst > 0
+
 
 def getEvents(pageToken=None):
     events = google_calendar.service.events().list(
@@ -10,7 +45,7 @@ def getEvents(pageToken=None):
         singleEvents=True,
         maxResults=1000,
         orderBy='startTime',
-        timeMin='2013-11-30T00:00:00-08:00', #str(datetime.now.isoformat()),
+        timeMin=str(datetime.now(LocalTimezone()).isoformat('T')),
         timeMax='2015-11-30T00:00:00-08:00',
         pageToken=pageToken,
         ).execute()
@@ -32,12 +67,12 @@ def getEvents(pageToken=None):
 # if __name__ == '__calendar_events__':
 #     calendar_events()
 
-print "Events: "
 events = getEvents()
 while True:
     for event in events['items']:
         pprint.pprint(event['summary'])
         pprint.pprint(event['start'])
+        print ''
     page_token = events.get('nextPageToken')
     if page_token:
         events = getEvents(page_token)
