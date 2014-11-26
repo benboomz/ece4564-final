5704841c3680e8f18354f3ee845c1f6e0d916a4f
bgeu <bene8@vt.edu>
2014-11-25 23:39:50 -0500

    web server not updating properly

--- webserver.py
+++ webserver.py
@@ -57,7 +57,7 @@ resource = ClockPage()
 factory = Site(resource)
 reactor.listenTCP(8888, factory)
 reactor.run() 
-'''
+
 
 from twisted.internet import reactor, task
 from twisted.web.server import Site
@@ -108,6 +108,42 @@ class RewriterResource(resource.Resource):
         if request.postpath and request.postpath[0][:1]=='~':
             request.postpath[:1] = ['users', request.postpath[0][1:]]
             request.path = '/'+'/'.join(request.prepath+request.postpath)
+ 
+resource = ClockPage()
+factory = Site(resource)
+reactor.listenTCP(8888, factory)
+reactor.run() 
+
+'''
+
+from twisted.internet import reactor, task
+from twisted.web.server import Site
+from twisted.web import server
+from twisted.web.resource import Resource
+import time
+ 
+class ClockPage(Resource):
+    isLeaf = True
+    def __init__(self):
+        self.presence=[]
+        loopingCall = task.LoopingCall(self.__print_time)
+        loopingCall.start(1, False)
+        Resource.__init__(self)
+     
+    def render_GET(self, request):
+        with open ("html.txt", "r") as myfile:
+            data=myfile.read()
+        old = data.replace("sleepcycles", str(time.ctime()))
+        request.write(old)
+        self.presence.append(request)
+        return server.NOT_DONE_YET
+     
+    def __print_time(self):
+        for p in self.presence:
+            with open ("html.txt", "r") as myfile:
+                data=myfile.read()
+            old = data.replace("sleepcycles", str(time.ctime()))
+            p.write(old)
  
 resource = ClockPage()
 factory = Site(resource)
