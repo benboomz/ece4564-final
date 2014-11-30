f614005e390b044ac585b2989a83efc376f3dad9
bgeu <bene8@vt.edu>
2014-11-30 17:34:59 -0500

    time

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pygame
+'''import pygame
 
 pygame.mixer.init()
 pygame.mixer.music.load("alarm_beep.wav")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
 	continue
+'''
+
+import time
+
+time.ctime()
+time = time.strftime('%m/%d/%y%l:%M %p') #11/30/14 5:34 PM
+print time
