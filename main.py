c8eb84c2160b841720c118ae0553e37c5ca33d99
bgeu <bene8@vt.edu>
2014-11-30 19:13:35 -0500

    testing alarms

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-'''import pygame
-
-pygame.mixer.init()
-pygame.mixer.music.load("alarm_beep.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-	continue
-'''
-
+import pygame
 import time
 
-time.ctime()
-time = time.strftime('%m/%d/%y%l:%M %p') #11/30/14 5:34 PM
-print time
+with open("alarms.txt") as f:
+	alarmlist = f.readlines()
+
+
+while 1:
+	time.ctime()
+	time = time.strftime('%m/%d/%y%l:%M %p') #11/30/14 5:34 PM
+	print time
+
+	for alarms in alarmlist:
+		if alarms == time:
+			pygame.mixer.init()
+			pygame.mixer.music.load("alarm_beep.wav")
+			pygame.mixer.music.play()
+			while pygame.mixer.music.get_busy() == True:
+				continue
+
