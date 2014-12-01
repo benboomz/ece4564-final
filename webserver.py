1ad48da6aa5a6022125798113083da5aa9a1691c
bgeu <bene8@vt.edu>
2014-12-01 08:52:24 -0500

    added child page for alarms in web server

--- webserver.py
+++ webserver.py
@@ -1,3 +1,89 @@
+
+from twisted.internet import reactor, task
+from twisted.web.server import Site
+from twisted.web import server, static
+from twisted.web.resource import Resource
+import datetime
+import time
+import json
+ 
+class ClockPage(Resource):
+    isLeaf = 0
+    def __init__(self):
+        self.presence=[]
+        Resource.__init__(self)
+
+    def render_GET(self, request):
+        with open ("home.html", "r+") as myfile:
+            data=myfile.read()
+        
+        with open("sleephistory.json", "r+") as historyfile:
+            sleep_history = json.load(historyfile)
+            html = ""
+            
+            for day in sorted(sleep_history.keys()):
+                html += '''<div class="row"><div class="col-md-2 date">''' + day + "</div>"
+
+                for time in sorted(sleep_history[day].keys()):
+
+                    html += '''<div class="col-md-1">
+                                <div class="circle circle-border"'''
+
+                    status = sleep_history[day][time]
+                    if status == "awake":
+                        html += '''style="border: 2px solid #FF0000;">'''
+                    elif status == "light sleep":
+                        html += '''style="border: 2px solid #FFFF00;">'''
+                    elif status == "rem sleep":
+                        html += '''style="border: 2px solid #31B404;">'''
+
+                    html += '''<div class="circle-inner"><div class="hour-text">''' + time
+                    html += '''             </div>
+                                        </div>
+                                    </div>
+                                </div>'''
+                html += "</div>"
+
+        new = data.replace("sleepcycles", str(html))
+        request.write(new)
+        self.presence.append(request)
+        request.finish()
+        return server.NOT_DONE_YET
+
+    def getChild(self, path, request):
+        if path == '':
+            return self
+        elif path == 'alarms.html':
+            return AlarmPage()
+
+class AlarmPage(Resource):
+    isLeaf = 1
+
+    def __init__(self):
+        self.presence=[]
+        Resource.__init__(self)
+
+    def render_GET(self, request):
+        with open ("alarms.html", "r+") as myfile:
+            data=myfile.read()
+
+        return data
+
+
+root = Resource()
+root.putChild('', ClockPage())
+root.putChild("style.css", static.File("style.css"))
+root.putChild("bootstrap", static.File("./bootstrap"))
+root.putChild('alarms.html', AlarmPage())
+factory = Site(root)
+reactor.listenTCP(8888, factory)
+reactor.run() 
+
+
+
+
+
+
 '''from twisted.internet import reactor
 from twisted.internet import task
 from twisted.web.server import Site
@@ -115,64 +201,3 @@ reactor.listenTCP(8888, factory)
 reactor.run() 
 
 '''
-
-from twisted.internet import reactor, task
-from twisted.web.server import Site
-from twisted.web import server, static
-from twisted.web.resource import Resource
-import datetime
-import time
-import json
- 
-class ClockPage(Resource):
-    isLeaf = True
-    def __init__(self):
-        self.presence=[]
-        Resource.__init__(self)
-
-    def render_GET(self, request):
-        with open ("home.html", "r+") as myfile:
-            data=myfile.read()
-        
-        with open("sleephistory.json", "r+") as historyfile:
-            sleep_history = json.load(historyfile)
-            html = ""
-            
-            for day in sorted(sleep_history.keys()):
-                html += '''<div class="row"><div class="col-md-2 date">''' + day + "</div>"
-
-                for time in sorted(sleep_history[day].keys()):
-
-                    html += '''<div class="col-md-1">
-                                <div class="circle circle-border"'''
-
-                    status = sleep_history[day][time]
-                    if status == "awake":
-                        html += '''style="border: 2px solid #FF0000;">'''
-                    elif status == "light sleep":
-                        html += '''style="border: 2px solid #FFFF00;">'''
-                    elif status == "rem sleep":
-                        html += '''style="border: 2px solid #31B404;">'''
-
-                    html += '''<div class="circle-inner"><div class="hour-text">''' + time
-                    html += '''             </div>
-                                        </div>
-                                    </div>
-                                </div>'''
-                html += "</div>"
-
-
-        new = data.replace("sleepcycles", str(html))
-        request.write(new)
-        self.presence.append(request)
-        request.finish()
-        return server.NOT_DONE_YET
-
-
-root = Resource()
-root.putChild('', ClockPage())
-root.putChild("style.css", static.File("style.css"))
-root.putChild("bootstrap", static.File("./bootstrap"))
-factory = Site(root)
-reactor.listenTCP(8888, factory)
-reactor.run() 
