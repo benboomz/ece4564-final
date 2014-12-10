cfabdc4625beb1a6480d72d85b4567a29abeb678
ben <bene8@vt.edu>
2014-12-09 20:01:52 -0500

    determined threshold for accelerometer, prints green, yellow, red accordingly for every ten seconds

--- accelerometertest.py
+++ accelerometertest.py
@@ -11,7 +11,7 @@ zsum = 0.0
 
 minx = miny = minz = 100
 maxx = maxy = maxz = -100
-
+print
 
 while 1:    
 	axes = adxl345.getAxes(True)
@@ -55,17 +55,39 @@ while 1:
 		maxy = round(maxy, 2)
 		maxz = round(maxz, 2)
 		
+		rangex = maxx - minx
+		rangey = maxy - miny
+		rangez = maxz - minz
+
+		avgrange = round((rangex+rangey+rangez)/3, 2)
 		
+		if avgrange > 0.50:
+			print "red"
+		elif avgrange < 0.20:
+			print "green"
+		else:
+			print "yellow"
+
+		print "\t" + str(avgrange)
+		print "\t" + str(rangex), str(rangey), str(rangez)
+		print		
+				
+		'''
 		if str(xavg) == "0.0":
 			print "x = " + str(xavg) + "\t\tmin: " + str(minx) + "\tmax: " + str(maxx) + "\trange: " + str(maxx-minx)
 		else:
 			print "x = " + str(xavg) + "\tmin: " + str(minx) + "\tmax: " + str(maxx) + "\trange: " + str(maxx-minx)
+		if str(yavg) == "0.0":
+			print "y = " + str(yavg) + "\t\tmin: " + str(miny) + "\tmax: " + str(maxy) + "\trange: " + str(maxy-miny)
+		else:
+			print "y = " + str(yavg) + "\tmin: " + str(miny) + "\tmax: " + str(maxy) + "\trange: " + str(maxy-miny)
 
-		print "y = " + str(yavg) + "\tmin: " + str(miny) + "\tmax: " + str(maxy)
-		print "z = " + str(zavg) + "\tmin: " + str(minz) + "\tmax: " + str(maxz)
+		print "z = " + str(zavg) + "\tmin: " + str(minz) + "\tmax: " + str(maxz) + "\trange: " + str(maxz-minz)
 		print
-		
+		'''
+
 		xsum = ysum = zsum = 0
 		minx = miny = minz = 100
 		maxx = maxy = maxz = -100
+
 	time.sleep(0.1)	
